7049eac35e198d0d6a7b740a345263dbf20b7a3a
Abhishek Kumar <readabhishek@gmail.com>
2021-02-10 00:37:56 -0500

    readMe updated

--- src/components/Level2Component.jsx
+++ src/components/Level2Component.jsx
@@ -11,7 +11,7 @@ export default function Level2Component (props) {
     /* use React Hook - 'useContext' and pass the context we created in Parent App.js component. This 'useContext' function returns
        the 'value' passed under 'context.Provider. The 'value' here is stored in the stateList (keeping same name as passed within
        'context.Provider'). The stateList can be used as required. If state changes, the components re-renders    */
-    
+
     const stateList = useContext(stateListContext);
     return (
         <div>
